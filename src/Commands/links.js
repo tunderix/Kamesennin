feef06c31480809cb7176018f3a18a25f125db1e
srepo <sami.repo@valamis.com>
2019-03-22 14:53:03 +0200

    Added keywords to links. Might be working

--- src/Commands/links.js
+++ src/Commands/links.js
@@ -2,34 +2,70 @@ import Command from '../Models/Command'
 import * as links from '../Data/links.json';
 import { embedMessageAuthor, embedMessageColor } from '../Utils/Constants'
 
+const givenKeyword = "";
+
 function Links () {
     Command.call(this);
 
     const header = "De Might be usefull for ya!";
 
     this.syntaxIdentifier = ['links'];
+    this.commandParameters = [
+        {
+            syntaxIdentifier: "atlas",
+            action: function() { 
+                givenKeyword = "atlas";
+            }
+        },
+        {
+            syntaxIdentifier: "terraria",
+            action: function() { 
+                givenKeyword = "terraria";
+            }
+        },
+        {
+            syntaxIdentifier: "dota",
+            action: function() { 
+                givenKeyword = "dota";
+            }
+        }
+    ];
+
+
     this.embeddedMessage = {
         color: embedMessageColor,
         author: {
           name: embedMessageAuthor
         },
         title: header,
-        fields: linkData
+        fields: linksWithKeyword(givenKeyword)
     };
 }
 
-const linkData = (links) => {
-    const linkData = [];
-    links.forEach(link => {
-        const newLink = {
-            name: link.header,
-            value: link.url,
-            inline: true
+const linksWithKeyword = (keyword) => {
+    // Check if there is data for the wanted keyword.
+    const linkData = null;
+    links.forEach(linkCategory => {
+        if(linkCategory.keyword === keyword){
+            linkData = linkCategory;
         }
-
-        linkData.push(newLink);
     });
-    return linkData;
+
+    // Go through all of the links in the linkdata and create new link elements for embedded message
+    const rLinks = [];
+    if(linkData != null){
+        const links = linkData.links;
+        links.forEach(link => {
+            const newLink = {
+                name: link.header,
+                value: link.url,
+                inline: true
+            }
+    
+            rLinks.push(newLink);
+        });
+    }
+    return rLinks;
 }
 
 Links.prototype = Object.create(Command.prototype);
